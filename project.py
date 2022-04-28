454866e4fcfd1e3c35745de597e5f3b19dbe309e
Ben Kaufmann <benk01@hotmail.com>
2022-04-28 16:14:10 -0700

    Added tf-idf

--- project.py
+++ project.py
@@ -4,7 +4,13 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+from IPython.display import Markdown, display
 
 def main():
     train_data = pd.read_csv("./train.csv")
@@ -18,11 +24,51 @@ def main():
     train_data["comment_text"] = train_data["comment_text"].apply(cleanComment)
     train_data["comment_text"] = train_data["comment_text"].apply(removeStopWords)
     train_data["comment_text"] = train_data["comment_text"].apply(stemComment)
-    print(train_data.head(15))
+
+    train, test = train_test_split(train_data, random_state=42, test_size=0.30, shuffle=True)
+
+    print(train.shape)
+    print(test.shape)
+
+    train_text = train['comment_text'].apply(lambda x: ' '.join(x))
+    test_text = test['comment_text'].apply(lambda x: ' '.join(x))
+
+    vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,3), norm='l2')
+    vectorizer.fit(train_text)
+    vectorizer.fit(test_text)
+    x_train = vectorizer.transform(train_text)
+    y_train = train.drop(labels = ['id','comment_text'], axis=1)
+
+    x_test = vectorizer.transform(test_text)
+    y_test = test.drop(labels = ['id','comment_text'], axis=1)
+    print(x_train)
+    #uniqueWords = train_data["comment_text"].explode().unique()
+    #for row in train_data["comment_text"]:
+
+    #uniqueWords = set(train_data["comment_text"])
+    #for line in train_data["comment_text"]:
+    LogReg_pipeline = Pipeline([
+                ('clf', OneVsRestClassifier(LogisticRegression(solver='sag'), n_jobs=-1)),
+            ])
+
+    for category in labels:
+        
+        # Training logistic regression model on train data
+        LogReg_pipeline.fit(x_train, train[category])
+        
+        # calculating test accuracy
+        prediction = LogReg_pipeline.predict(x_test)
+        print('Test accuracy is {}'.format(accuracy_score(test[category], prediction)))
+        print("\n")
+        
 
 
 
-
+def tf_idf(text):
+    vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,3), norm='l2')
+    vectorizer.fit(text)
+    x = vectorizer.transform(text)
+    return x
 
 # cleans comment by setting it to lower case and removing non-alphabetic/non-numeric characters
 def cleanComment(comment):
